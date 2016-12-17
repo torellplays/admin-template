7a062120fdae0d6766d0b3eac15d4588f61e109c
Mariusz Klawikowski <torellplays@gmail.com>
2016-12-17 13:32:52 +0100

    added dashboard.component

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dashboard',
+  styles: [require('./dashboard.css')],
+  template: require('./dashboard.html')
+})
+export class Dashboard {
+
+  constructor() {
+  }
+
+}
