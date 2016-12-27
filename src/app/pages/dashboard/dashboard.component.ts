c413d550f6e2e558cbe2887d67889166b4511f1f
Mariusz Klawikowski <torellplays@gmail.com>
2016-12-27 09:32:56 +0100

    removed 'require'

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'dashboard',
-  styles: [require('./dashboard.css')],
-  template: require('./dashboard.html')
+  templateUrl: './dashboard.html'
 })
 export class Dashboard {
 
