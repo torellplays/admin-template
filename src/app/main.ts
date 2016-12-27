3c02a305286b0b20047ca6d8dae68cedad909152
Mariusz Klawikowski <torellplays@gmail.com>
2016-12-27 10:27:23 +0100

    added enableProdMode

--- src/app/main.ts
+++ src/app/main.ts
@@ -4,5 +4,7 @@ import 'zone.js/dist/zone';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule }              from './app.module';
+import { enableProdMode } from '@angular/core';
+
 
 platformBrowserDynamic().bootstrapModule(AppModule);
