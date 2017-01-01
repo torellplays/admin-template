58a4279ea00fa3c4f66cd15c5836db13890dbb29
Mariusz Klawikowski <torellplays@gmail.com>
2017-01-01 19:55:43 +0100

    fix app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app',
-	template: '<h1>admin template soon<h1>'
+	template: `
+		<dashboard></dashboard>
+	`
 })
 
 export class AppComponent {}
