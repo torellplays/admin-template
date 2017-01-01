c02dcc1c4abba08cca06a04bd030d7da3b6b395d
Mariusz Klawikowski <torellplays@gmail.com>
2017-01-01 20:00:25 +0100

    add loaders

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,14 @@ module.exports = {
 	},
 	module: {
 	 loaders: [
-	   {test: /\.ts$/, loader: 'ts-loader'},
-		 {test: /\.html$/, loader: 'html-loader'}
+	   {test: /\.ts$/, loaders: ['ts-loader', 'angular2-template-loader?keepUrl=true', 'angular2-router-loader']},
+		 {test: /\.html$/, loader: 'html-loader'},
+		 {test: /\.(jpe?g|png|gif|svg)$/i, loaders: [
+      'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+      'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false']},
+		 { test: /\.css$/, loader: "style-loader!css-loader?root=." },
+		 {test: /\.js$/, loader: 'script-loader'}
+
 	]
 	},
 	resolve: {
