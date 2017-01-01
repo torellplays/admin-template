93c4a132546f7bf52772e2be0ec208e413e2f2d2
Mariusz Klawikowski <torellplays@gmail.com>
2017-01-01 19:56:04 +0100

    fix app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { RouterModule }   from '@angular/router';
 
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, DashboardComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
