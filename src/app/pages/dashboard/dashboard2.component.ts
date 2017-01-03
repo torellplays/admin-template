85609dc44d99b1c15bbf7a20739fb5be5b3b4095
Mariusz Klawikowski <torellplays@gmail.com>
2017-01-03 12:42:51 +0100

    add dashboard2 component

---
+++ src/app/pages/dashboard/dashboard2.component.ts
@@ -0,0 +1,9 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dashboard2',
+  template: require('./dashboard2.component.html')
+})
+export class Dashboard2Component {
+}
