0db9c67348a8bb37ba9a815e5473e147c8e16c1a
Mariusz Klawikowski <torellplays@gmail.com>
2017-01-03 12:41:09 +0100

    add dashboard2.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Dashboard2Component } from './pages/dashboard/dashboard2.component';
 import { routing } from './app.routing';
 
 
 @NgModule({
   imports:      [ BrowserModule, routing ],
-  declarations: [ AppComponent, DashboardComponent ],
+  declarations: [ AppComponent, DashboardComponent, Dashboard2Component ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
