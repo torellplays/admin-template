4df9f3dd95caae476e5d4752a36736ac632dd7d7
Mariusz Klawikowski <torellplays@gmail.com>
2017-01-03 11:42:34 +0100

    remove dashboard component from src/app

--- src/app/dashboard.component.ts
+++
@@ -1,9 +0,0 @@
-
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'dashboard',
-  template: require('./pages/dashboard/dashboard.component.html')
-})
-export class DashboardComponent {
-}
