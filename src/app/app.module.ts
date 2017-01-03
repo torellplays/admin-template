ad9451ea69807ad22c12eed94f951f4ee8471bcf
Mariusz Klawikowski <torellplays@gmail.com>
2017-01-03 11:41:53 +0100

    change path do dashboardcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
-import { DashboardComponent } from './dashboard.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { routing } from './app.routing';
 
 
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 
-import { DashboardComponent } from './dashboard.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
+{ path: 'dashboard', component: DashboardComponent },
+{ path: '' , redirectTo: '/dashboard', pathMatch: 'full' }
+
 ];
 
 export const routing = RouterModule.forRoot(routes);
