4a1502fe24e43a2d1d91e29a4f486c5aa07d5526
Mariusz Klawikowski <torellplays@gmail.com>
2017-01-03 12:41:28 +0100

    add route to dashboard2

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Dashboard2Component } from './pages/dashboard/dashboard2.component';
+
 
 const routes: Routes = [
 { path: 'dashboard', component: DashboardComponent },
+{ path: 'dashboard2', component: Dashboard2Component },
 { path: '' , redirectTo: '/dashboard', pathMatch: 'full' }
 
 ];
