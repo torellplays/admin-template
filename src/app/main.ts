8a92e7223591e4d22eb645acccdffbc9271d1fd5
Mariusz Klawikowski <torellplays@gmail.com>
2017-01-03 10:50:37 +0100

    add functions

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,10 +1,25 @@
-import 'core-js';
 import 'reflect-metadata';
-import 'zone.js/dist/zone';
-
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule }              from './app.module';
-import { enableProdMode } from '@angular/core';
+
+import 'core-js';
+import 'zone.js/dist/zone';
+
+
+if (process.env.ENV === 'build') {
+  enableProdMode();
+}
+
+export function main() {
+  return platformBrowserDynamic().bootstrapModule(AppModule);
+}
+
+if (document.readyState === 'complete') {
+  main();
+} else {
+  document.addEventListener('DOMContentLoaded', main);
+}
 
 
 platformBrowserDynamic().bootstrapModule(AppModule);
