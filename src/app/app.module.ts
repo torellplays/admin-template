a721be1adb490ae68d61b041d797e3b5943dcf46
Mariusz Klawikowski <torellplays@gmail.com>
2017-01-04 18:28:55 +0100

    add data and simple tables components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { AppComponent }  from './app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Dashboard2Component } from './pages/dashboard/dashboard2.component';
 import { WidgetsComponent } from './pages/widgets.component';
+import { DataTablesComponent } from './pages/tables/data-tables.component';
+import { SimpleTablesComponent } from './pages/tables/simple-tables.component';
 import { routing } from './app.routing';
 
 
 @NgModule({
   imports:      [ BrowserModule, routing ],
-  declarations: [ AppComponent, DashboardComponent, Dashboard2Component, WidgetsComponent ],
+  declarations: [ AppComponent, DashboardComponent, Dashboard2Component, WidgetsComponent,
+                  DataTablesComponent, SimpleTablesComponent
+                ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Dashboard2Component } from './pages/dashboard/dashboard2.component';
 import { WidgetsComponent } from './pages/widgets.component';
+import { DataTablesComponent } from './pages/tables/data-tables.component';
+import { SimpleTablesComponent } from './pages/tables/simple-tables.component';
 
 const routes: Routes = [
 { path: 'dashboard', component: DashboardComponent },
 { path: 'dashboard2', component: Dashboard2Component },
 { path: 'widgets', component: WidgetsComponent },
+{ path: 'datatables', component: DataTablesComponent },
+{ path: 'simpletables', component: SimpleTablesComponent },
 { path: '' , redirectTo: '/dashboard', pathMatch: 'full' }
 
 ];
