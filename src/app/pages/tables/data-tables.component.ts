35acfae184b8dfcb6d4691a19053f6f6cf0e3c1f
Mariusz Klawikowski <torellplays@gmail.com>
2017-01-04 18:31:24 +0100

    first commit

---
+++ src/app/pages/tables/data-tables.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'data-tables',
+  template: require('./data-tables.component.html')
+})
+export class DataTablesComponent {
+}
---
+++ src/app/pages/tables/simple-tables.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'simple-tables',
+  template: require('./simple-tables.component.html')
+})
+export class SimpleTablesComponent {
+}
