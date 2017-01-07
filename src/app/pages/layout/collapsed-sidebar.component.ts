15a32db76c8f2092e449ea92570d223bb8908bb5
Mariusz Klawikowski <torellplays@gmail.com>
2017-01-07 13:32:39 +0100

    add collapsed-sidebar component

---
+++ src/app/pages/layout/collapsed-sidebar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'collapsed-sidebar',
+  template: require('./collapsed-sidebar.component.html')
+})
+export class CollapsedSidebarComponent {
+}
