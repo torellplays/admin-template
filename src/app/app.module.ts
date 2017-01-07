c66bce81c352e3edea66776dcba7b93cfb5b133c
Mariusz Klawikowski <torellplays@gmail.com>
2017-01-07 12:35:17 +0100

    add top-nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,14 @@ import { Dashboard2Component } from './pages/dashboard/dashboard2.component';
 import { WidgetsComponent } from './pages/widgets.component';
 import { DataTablesComponent } from './pages/tables/data-tables.component';
 import { SimpleTablesComponent } from './pages/tables/simple-tables.component';
+import { TopNavComponent } from './pages/layout/top-nav.component';
 import { routing } from './app.routing';
 
 
 @NgModule({
   imports:      [ BrowserModule, routing ],
   declarations: [ AppComponent, DashboardComponent, Dashboard2Component, WidgetsComponent,
-                  DataTablesComponent, SimpleTablesComponent
+                  DataTablesComponent, SimpleTablesComponent, TopNavComponent
                 ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,6 +5,7 @@ import { Dashboard2Component } from './pages/dashboard/dashboard2.component';
 import { WidgetsComponent } from './pages/widgets.component';
 import { DataTablesComponent } from './pages/tables/data-tables.component';
 import { SimpleTablesComponent } from './pages/tables/simple-tables.component';
+import { TopNavComponent } from './pages/layout/top-nav.component';
 
 const routes: Routes = [
 { path: 'dashboard', component: DashboardComponent },
@@ -12,6 +13,7 @@ const routes: Routes = [
 { path: 'widgets', component: WidgetsComponent },
 { path: 'datatables', component: DataTablesComponent },
 { path: 'simpletables', component: SimpleTablesComponent },
+{ path: 'top-nav', component: TopNavComponent },
 { path: '' , redirectTo: '/dashboard', pathMatch: 'full' }
 
 ];
